daf7c8baf309bf2eaec497a5ad44800c86e2445a
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-12-14 11:29:41 -0300

    creating first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { CompaComponent } from './compa/compa.component';
+import { CompbComponent } from './compb/compb.component';
+import { CompcComponent } from './compc/compc.component';
+import { GokuComponent } from './goku/goku.component';
+import { GohanComponent } from './gohan/gohan.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent,
+    CompaComponent,
+    CompbComponent,
+    CompcComponent,
+    GokuComponent,
+    GohanComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
